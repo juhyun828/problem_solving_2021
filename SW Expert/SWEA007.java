cfc8706f728a979e663620fd4a83e564d900a7b5
jhk828 <j828hh@gmail.com>
2021-01-29 23:33:12 +0900

    210129

---
+++ SW Expert/SWEA007.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SWEA007 {
+	// 210129
+	// 2007. 패턴 마디의 길이
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		
+		int N = sc.nextInt();
+		
+		for (int tc = 1; tc<=N; tc++) {
+			char[] arr = sc.next().toCharArray();
+			
+			int j=1; // 길이는 1부터 시작
+			for (int i=0; i<arr.length-1; i++) {
+				// arr[0] == arr[j]
+				// arr[1] == arr[j+1] 
+				// .....
+				for(; j<arr.length-1-i; j++) {
+					if (arr[i] == arr[i+j]) {
+						break;
+					}
+				}
+			}
+			
+			System.out.println("#" + tc + " " + j);
+		} // for
+		
+		sc.close();	
+	} // main
+}
---
+++ SW Expert/SWEA1926.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SWEA1926 {
+	// 210129
+	// 1926. 간단한 369게임
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			int d = 1;
+			if (i >= 10 && i < 100)
+				d = 10;
+			else if (i >= 100 && i < 1000)
+				d = 100;
+			else if (i == 1000)
+				d = 1000;
+
+			boolean flag = false;
+			int tmp = i;
+			while (d > 0) {
+				int num = tmp / d;
+				if (num == 3 || num == 6 || num == 9) {
+					flag = true;
+					System.out.print("-");
+				}
+				tmp -= num * d;
+				d /= 10;
+			} // while
+			if (!flag)
+				System.out.print(i);
+			System.out.print(" ");
+
+		} // for
+		sc.close();
+	} // main
+}
