51ee4535df6d35fc335c8a9a3332fcf9599d30cc
jhk828 <j828hh@gmail.com>
2021-03-02 22:45:48 +0900

    210302 알고리즘

--- BJ/Main_BJ_7576_토마토.java
+++ BJ/Main_BJ_7576_토마토.java
@@ -1,77 +1,85 @@
 import java.io.*;
 import java.util.*;
+// 0302
 
-// 210212
 public class Main_BJ_7576_토마토 {
+	static int N, M, map[][], dist[][];
+	static Queue<Tomato> q;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 	
-	static int M, N;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[][] days;
-	static int[] dr = {-1, 0, 0, 1};
-	static int[] dc = {0, -1, 1, 0};
-	static Queue<Integer[]> q;
+	static class Tomato {
+		int r, c;
+
+		public Tomato(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	static int stoi(String str) {
+		return Integer.parseInt(str);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input_BJ_7576_토마토.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	
+		M = stoi(st.nextToken()); // 가로
+		N = stoi(st.nextToken()); // 세로
+		
+		map = new int[N][M]; 
+		dist = new int[N][M]; 
+		
+		for(int r=0; r<N; r++) {
+			Arrays.fill(dist[r], -1);
+		}
+		
+		q = new ArrayDeque();
+		
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int c=0; c<M; c++) {
+				map[r][c] = stoi(st.nextToken());
+				if(map[r][c]==1) {
+					q.add(new Tomato(r, c));
+					dist[r][c] = 0;
+				}
+			}
+		}
+		
+		bfs();
+		System.out.println(check());
+		br.close();
+	} // main
+
+	static void bfs() {
+		while(!q.isEmpty()) {
+			Tomato t = q.poll();
+			int r = t.r, c = t.c;
+			
+			for(int d=0; d<4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				if(nr<0||nr>=N||nc<0||nc>=M||map[nr][nc]!=0) continue;
+				map[nr][nc] = 1;
+				dist[nr][nc] = dist[r][c] + 1;
+				q.offer(new Tomato(nr, nc));
+			}
+			
+		}
+	}
 	
 	static int check() {
 		int max = 0;
 		for(int r=0; r<N; r++) {
 			for(int c=0; c<M; c++) {
-				if (map[r][c]==0)
-					return -1;
-				else if (map[r][c]==1) {
-					max = Math.max(max, days[r][c]);
-				}
+				if(map[r][c]==0) return -1;
+				max = Math.max(max, dist[r][c]);
 			}
 		}
 		return max;
 	}
-	
-	static void bfs() {
-		
-		while (!q.isEmpty()) {
-			Integer[] v = q.poll();
-			for(int d=0; d<4; d++) {
-				int nr = v[0] + dr[d];
-				int nc = v[1] + dc[d];
-				
-				if (nr>=0 && nr<N && nc>=0 && nc<M 
-						&& map[nr][nc]==0 && !visited[nr][nc]) {
-					visited[nr][nc] = true;
-					map[nr][nc] = 1;
-					days[nr][nc] = days[v[0]][v[1]] + 1;
-					q.offer(new Integer[] {nr, nc});
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/input_BJ_7576_토마토.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		 
-		M = Integer.parseInt(st.nextToken()); 
-		N = Integer.parseInt(st.nextToken()); 
-		
-		map =  new int[N][M];
-		days = new int[N][M];
-		visited = new boolean[N][M];
-		q = new ArrayDeque<Integer[]>();
-		
-		for(int r=0; r<N; r++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for(int c=0; c<M; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken()); 
-				if (map[r][c]==1) {
-					q.offer(new Integer[] {r, c});
-					visited[r][c] = true;
-				}
-			}
-		} 
-		
-		bfs();
-		System.out.println(check());
-		
-		br.close(); 
-	} // main
 }
