f24f3820a0a7e557f42068a6f854f48e7ac70720
jhk828 <j828hh@gmail.com>
2021-04-10 23:49:16 +0900

    210410 알고리즘

--- BJ/Main_BJ_17471_게리멘더링.java
+++ BJ/Main_BJ_17471_게리멘더링.java
@@ -1,119 +1,116 @@
 import java.io.*;
 import java.util.*;
-// 210318
+// 210410
 
 public class Main_BJ_17471_게리멘더링 {
-	static int N; // 도시 수
-	static int[] p; // 인구 수
-	static int[][] map; // 도시 연결 관리 -> 인접 행렬
-	static int[] teams;
-	static int min = Integer.MAX_VALUE;
-	
-	static int stoi(String str) {
-		return Integer.parseInt(str);
-	}
-	
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("res/input_BJ_17471_게리멘더링.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
-		
-		N = stoi(br.readLine().trim());
-		p = new int[N+1];
-		map = new int[N+1][N+1];
-		teams = new int[N+1];
-		
-		st = new StringTokenizer(br.readLine().trim(), " ");
-		for(int i=1; i<=N; i++) {
-			p[i] = stoi(st.nextToken());
-		}
-		
-		int cnt=0;
-		int k;
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine().trim(), " ");
-			cnt = stoi(st.nextToken());
-			for(int j=0; j<cnt; j++) {
-				k = stoi(st.nextToken()); // i->k 연결
-				map[i][k] = 1;
-				map[k][i] = 1;
-			}
-		}
-		
-		// 솔루션
-		dfs(1); // N=1부터 시작
-		
-		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
-		br.close();
-	}
-	
-	// 현재 위치의 도시를 왼쪽에 포함시키거나 / 오른쪽에 포함시키거나
-	static void dfs(int idx) { 
-		if (idx == (N+1)) { // N=1부터 시작하기 때문에
-			// 솔루션 구현
-			// A구역, B구역이 각각 연결되어 있는지
-			if (check(0) && check(1)) { // 0번들 / 1번들 끼리 연결되어 있나
-				int r = getCount(); // 두 도시의 인구수 차이 최소값
-				min = Math.min(min, r);
-			} 
-			return;
-		}
-		
-		teams[idx] = 0; // idx번째를 A팀에
-		dfs(idx+1);
-		
-		teams[idx] = 1; // idx를 B팀에
-		dfs(idx+1);
-	}
-	
-	static boolean check(int type) { // 연결되는지 확인 -> bfs
-		boolean[] v = new boolean[N+1];
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		
-		for(int i=1; i<=N; i++) {
-			if(teams[i] == type) {
-				v[i] = true;
-				q.offer(i);
-				break; // 첫번째로 연결된 구역만 찾는다.
-			}
-		}
-		
-		if(q.size()==0) {
-			// 각 선거구는 적어도 하나의 구역을 포함해야 한다.
-			return false;
-		}
-	
-		int cur;
-		while(!q.isEmpty()) {
-			cur = q.poll();
-			for(int i=1; i<=N; i++) { 
-				// 연결이 있을 때만 간다.
-				if(map[cur][i]==0) continue; // 연결x
-				if(v[i]) continue; // 이미 방문/연결된 구역
-				if(teams[i]!=type) continue; // 다른 팀
-				v[i] = true; // 팀이 같은 i번째는 모두 true여야 한다.
-				q.offer(i); // 구역 방문
-			}
-		}
-		
-		for(int i=1; i<=N; i++) {
-			if(teams[i]!=type) continue;
-			if(!v[i]) return false; 
-		}
-		
-		return true;
-	}
-	
-	static int getCount() {
-		int team1 = 0;
-		int team2 = 0;
-		
-		for(int i=1; i<=N; i++) {
-			if(teams[i]==0) team1 += p[i];
-			else team2 += p[i];
-		}
-		
-		return Math.abs(team1-team2);
-	}
+    static int N, min;
+    static ArrayList<Integer>[] adjList;
+    static int[] population, teams;
+
+    // 1. 구역 두개로 나누기
+    static void dfs(int cnt, int L) {
+        if(L==N+1) { // 1부터 시작
+            if(cnt==0 || cnt==N) return;
+
+            // 2. 각 구역이 연결되었는지 확인
+            if(bfs(1) && bfs(2)) {
+                // 3. min 갱신
+                min = Math.min(min, getCount());
+            }
+            return;
+        }
+
+        teams[L] = 1;
+        dfs(cnt+1, L+1);
+        teams[L] = 2;
+        dfs(cnt, L+1);
+    }
+
+    // 2. 각 구역이 연결되었는지 확인
+    static boolean bfs(int type) {
+        boolean[] v = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<Integer>();
+
+        // type번 구역의 제일 첫 지역 번호 넣기
+        for(int i=1; i<=N; i++) {
+            if(teams[i] == type) {
+                q.offer(i);
+                v[i] = true;
+                break;
+            }
+        }
+
+        int cur;
+        // 탐색 시작
+        while(!q.isEmpty()) {
+            cur = q.poll();
+
+            for(int next: adjList[cur]) {
+                if(v[next]) continue;;
+                if(teams[next]!=type) continue;
+                q.offer(next);
+                v[next] = true;
+            }
+        }
+
+        // 해당 지역을 모두 탐색했는지 확인
+        for(int i=1; i<=N; i++) {
+            if(teams[i]!=type) continue;
+            if(!v[i]) return false;
+        }
+        return true;
+    }
+
+    // 3. min 갱신
+    static int getCount() {
+        int point1 = 0;
+        int point2 = 0;
+
+        for(int i=1; i<=N; i++) {
+            if(teams[i]==1) point1 += population[i];
+            else if(teams[i]==2) point2 += population[i];
+        }
+
+        return Math.abs(point1-point2);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/res/input_BJ_17471_게리멘더링.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = stoi(br.readLine());
+        min = Integer.MAX_VALUE;
+        population = new int[N+1]; // 1번 지역부터
+        teams = new int[N+1];
+
+        // 인구 수
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++) {
+            population[i] = stoi(st.nextToken());
+        }
+        
+        // 인접리스트
+        adjList = new ArrayList[N+1];
+        int l, to;
+        for(int i=1; i<=N; i++) {
+            adjList[i] = new ArrayList<Integer>();
+            st = new StringTokenizer(br.readLine());
+            l = stoi(st.nextToken());
+            for(int j=0; j<l; j++) {
+                to = stoi(st.nextToken());
+                adjList[i].add(to);
+            }
+        }
+
+        dfs(0, 1);
+
+        System.out.println(min==Integer.MAX_VALUE ? -1 : min);
+
+        br.close();
+    }
+
+    static int stoi(String str) {
+        return Integer.parseInt(str);
+    }
 }
