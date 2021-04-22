27861149c611a5ad936aaff5693d0269672f395f
jhk828 <j828hh@gmail.com>
2021-04-23 01:33:05 +0900

    update README

--- BJ/Main_BJ_20056_마법사상어와파이어볼.java
+++ BJ/Main_BJ_20056_마법사상어와파이어볼.java
@@ -1,144 +1,146 @@
 import java.io.*;
 import java.util.*;
-// 210415
+// 210423
 
 public class Main_BJ_20056_마법사상어와파이어볼 {
 	static int N, M, K;
-	// 0 1 2 3 4 5 6 7
-	static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
-	static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1 };
-	static ArrayList<int[]>[][] list; // fireballs 3차원으로 저장-> m, s, d
-
-	// 1.
-	static ArrayList<int[]>[][] move() {
-		ArrayList<int[]>[][] tmplist = new ArrayList[N+1][N+1];
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				tmplist[i][j] = new ArrayList<int[]>();
-			}
-		}
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(list[i][j].size()==0) continue; 
-				// 파이어 스톰이 있음
-				for(int k=0; k<list[i][j].size(); k++) {
-					int[] firestrom = list[i][j].get(k);
-					// 이동
-					int d = firestrom[2];
-					int nr = i + dr[d] * firestrom[1];
-					int nc = j + dc[d] * firestrom[1];
-				
-					while(!(nr>=1 && nr<=N && nc>=1 && nc<=N)) {	
-						if(nr>=N+1) nr-=N;
-						else if(nr<=0) nr+=N;
-						if(nc>=N+1) nc-=N;
-						else if(nc<=0) nc+=N;
-					}
-					
-					// 추가
-					tmplist[nr][nc].add(new int[] {firestrom[0], firestrom[1], firestrom[2]});
-				}
-
-			}
-		}
-		return tmplist;
-	
-	} // move
-	
-	// 2. 
-	static void divide() {
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(list[i][j].size()>=2) {
-					int mSum=0;
-					int sSum=0;
-					int dModSum=0; // 2로 나눈 값 나머지의 합이 0이거나 사이즈랑 같으면 모두 짝수/홀수
-								// 아니면 섞인것
-					
-					for(int k=0; k<list[i][j].size(); k++) {
-						mSum+=list[i][j].get(k)[0];
-						sSum+=list[i][j].get(k)[1];
-						dModSum+=list[i][j].get(k)[2]%2;
-					}
-					int na=mSum/5;
-					int ns=sSum/list[i][j].size();
-					boolean even = (dModSum==0 || dModSum==list[i][j].size()) ? true : false;
-					list[i][j] = new ArrayList<int[]>();
-					
-					if(na<=0) {
-						// 소멸되어 사라짐
-						continue;
-					}
-					
-					if(even) {
-						for (int d = 0; d <= 6; d += 2) {
-							list[i][j].add(new int[] {na, ns, d});
-						}
-					} else {
-						for (int d = 1; d <= 7; d += 2) {
-							list[i][j].add(new int[] {na, ns, d});
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	static int sum() {
-		int mSum = 0;
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(list[i][j].size()==0) continue; 
-				for(int k=0; k<list[i][j].size(); k++) {					
-					mSum+=list[i][j].get(k)[0];				
-				}
-			}
-		}
-		return mSum;
-	}
-	
-	static void solve() {
-		for(int t=1; t<=K; t++) {
-			list = move();
-			divide();
-		}
-		
-		System.out.println(sum());
-	}
+	static ArrayList<int[]>[][] map; // m, s, d 순서
+	static int[] dr = {-1, -1, 0, 1, 1, 1 , 0, -1};
+	static int[] dc = { 0, 1,  1, 1, 0, -1, -1, -1}; // // 0 1 2 3 4 5 6 7
 	
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input_BJ_20056_마법사상어와파이어볼.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	
+		N = stoi(st.nextToken());
+		M = stoi(st.nextToken());
+		K = stoi(st.nextToken());
 		
-		N = stoi(st.nextToken()); // 지도 크기
-		M = stoi(st.nextToken()); // fireball 개수
-		K = stoi(st.nextToken()); // 시간
-		
-		list = new ArrayList[N+1][N+1];
+		map = new ArrayList[N+1][N+1];
 		for(int i=1; i<=N; i++) {
 			for(int j=1; j<=N; j++) {
-				list[i][j] = new ArrayList<int[]>();
+				map[i][j] = new ArrayList<int[]>();
 			}
 		}
-
+		
+		// 파이어볼 정보 입력
 		int r, c, m, s, d;
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			r = stoi(st.nextToken());
 			c = stoi(st.nextToken());
-			m = stoi(st.nextToken());			
+			m = stoi(st.nextToken());
 			s = stoi(st.nextToken());
 			d = stoi(st.nextToken());
-			
-			list[r][c].add(new int[] {m, s, d});
+			map[r][c].add(new int[] {m, s, d});
 		}
 		
 		solve();
+
 		br.close();
 	}
+	
+	static void solve() {
+		for(int i=0; i<K; i++) {
+			map = move();
+			divide();
+		}
+		System.out.println(sumLeftM());
+	}
+	
+	// 1.
+	static ArrayList<int[]>[][] move() {
+		ArrayList<int[]>[][] newMap = new ArrayList[N+1][N+1];
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				newMap[i][j] = new ArrayList<int[]>();
+			}
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if(map[i][j].size()==0) continue;
+				
+				for(int z=0; z<map[i][j].size(); z++) {
+					int[] firestorm = map[i][j].get(z);
+					int m = firestorm[0];
+					int s = firestorm[1];
+					int d = firestorm[2];
+					
+					int nr = i + dr[d]*s;
+					int nc = j + dc[d]*s;
+					
+					while(nr<1 || nr>N || nc<1 || nc>N) {
+						if(nr<1) nr += N;
+						else if(nr>N) nr -=N;
+						if(nc<1) nc += N;
+						else if(nc>N) nc -=N;
+					}
+					
+					newMap[nr][nc].add(new int[] {m, s, d});
+				}
+			}
+		}
 
+		return newMap;
+	}
+	
+	// 2.
+	static void divide() {
+		// 여러 파이어볼이 들어있는 칸은 합친 후 4개로 쪼갠다.
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if(map[i][j].size()<=1) continue;
+				
+				int mSum = 0, sSum = 0, dModSum = 0;	
+				// 2로 나눈 값 나머지의 합이 0이거나 사이즈랑 같으면 모두 짝수/홀수
+				// 아니면 섞인것
+				for(int z=0; z<map[i][j].size(); z++) {
+					mSum += map[i][j].get(z)[0];
+					sSum += map[i][j].get(z)[1];
+					dModSum += map[i][j].get(z)[2]%2;
+				}
+				
+				int nm = mSum/5;
+				int ns = sSum/map[i][j].size();
+				boolean even = false;
+				if(dModSum==0 || dModSum==map[i][j].size()) even = true;
+				
+				map[i][j] = new ArrayList<int[]>();
+				if(nm<=0) continue; // 소멸되어 사라짐
+				else {
+					if(even) { // 짝수 방향들
+						for(int nd=0; nd<=6; nd+=2) {
+							map[i][j].add(new int[] {nm, ns, nd});
+						}
+						
+					} else { // 홀수 방향들
+						for(int nd=1; nd<=7; nd+=2) {
+							map[i][j].add(new int[] {nm, ns, nd});
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// 3. 남은 질량들 합
+	static int sumLeftM () {
+		int left=0;
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if(map[i][j].size()==0) continue;
+				
+				for(int z=0; z<map[i][j].size(); z++) {
+					left += map[i][j].get(z)[0];
+				}
+			}
+		}
+		return left;
+	}
+	
 	static int stoi(String str) {
 		return Integer.parseInt(str);
 	}
--- BJ/Main_BJ_2636_치즈.java
+++ BJ/Main_BJ_2636_치즈.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.util.*;
-// 210324
+// 210422
 
 public class Main_BJ_2636_치즈 {
-	static int H, W, time, cheeseCnt;
-	static int[][] map;
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
+	static int H, W, cheeseCnt; // 세로, 가로
+	static int[][] map; // 0: 빈칸, 1: 치즈
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, -1, 0, 1};
 	
-	static class Pos {
+	static class Pos{
 		int r, c;
 
 		public Pos(int r, int c) {
@@ -17,71 +17,76 @@ public class Main_BJ_2636_치즈 {
 		}
 	}
 	
-	static int stoi(String str) {
-		return Integer.parseInt(str);
-	}
-	
-	static void bfs() {
-		
-		Queue<Pos> q = new ArrayDeque<>();
-		boolean[][] v = new boolean[H][W];
-		
-		q.offer(new Pos(0, 0));
-		v[0][0] = true;
-		
-		Pos cur = null;
-		int nr, nc;
- 		while(!q.isEmpty()) {
-			cur = q.poll();
-			
-			for(int d=0; d<4; d++) {
-				nr = cur.r + dr[d]; 
-				nc = cur.c + dc[d];
-				if(nr<0 || nr>=H || nc<0 || nc>=W || v[nr][nc]) continue;
-				if(map[nr][nc]==1) { // 공기가 치즈와 만나면 녹임
-					map[nr][nc] = 0; 
-					--cheeseCnt;
-					v[nr][nc] = true; // 치즈를 녹인 곳도 방문처리 해야 다음 치즈가 공기로 인식 x
-				} 
-				else {
-					q.offer(new Pos(nr, nc)); // 치즈가 아니라 공기
-					v[nr][nc] = true;
-				}
-			}
-		}	
-	}
-	
 	public static void main(String[] args) throws Exception {
-		// System.setIn(new FileInputStream("res/input_BJ_2636_치즈.txt"));
+		//System.setIn(new FileInputStream("res/input_BJ_2636_치즈.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		H = stoi(st.nextToken());
 		W = stoi(st.nextToken());
 		map = new int[H][W];
-		time = 0; cheeseCnt = 0;
+		cheeseCnt = 0;
 		
-		for(int r=0; r<H; r++) {
+		for(int i=0; i<H; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			for(int c=0; c<W; c++) {
-				map[r][c] = stoi(st.nextToken());
-				if(map[r][c]==1) { // 치즈
-					++cheeseCnt;
-				}
+			for(int j=0; j<W; j++) {
+				map[i][j] = stoi(st.nextToken());
+				if(map[i][j]==1) ++cheeseCnt;
 			}
 		}
 		
-		int lastCnt = 0;
-		while(cheeseCnt>0) {
-			++time;
-			lastCnt = cheeseCnt;
-			bfs();
-			System.out.println(time + "\n" + lastCnt);
-			printMap();
-		}
-		
-		System.out.println(time + "\n" + lastCnt);
+		solve();
 		br.close();
 	}
+	
+	static void solve() {
+		int lastCnt = 0;
+		int time = 0;
+		
+		while(true) {
+			lastCnt = cheeseCnt;
+			++time;
+			bfs();
+			if(cheeseCnt==0) break;
+		}
+		
+		System.out.println(time);
+		System.out.println(lastCnt);
+	}
+	
+	static void bfs(){
+		// 공기를 만날 때 마다 녹여야 하기 때문에, 공기 칸인 0, 0에서 시작
+		// 공기 칸에서 시작하며 만나는 빈칸은 구멍이 아니라 공기다.
+		
+		boolean[][] v = new boolean[H][W];
+		ArrayDeque<Pos> q = new ArrayDeque<>();
+		v[0][0] = true;
+		q.offer(new Pos(0, 0));
+		
+		while(!q.isEmpty()) {
+			Pos cur = q.poll();
+		
+			for(int d=0; d<4; d++) {
+				int nr = cur.r + dr[d];
+				int nc = cur.c + dc[d];
+				
+				if(nr<0 || nr>=H || nc<0 || nc>=W || v[nr][nc]) continue;
+				if(map[nr][nc]==0) { // 만나게 되는 공기는 다음 탐색 지점
+					v[nr][nc] = true;
+					q.offer(new Pos(nr, nc));
+					
+				} else if(map[nr][nc]==1) { // 치즈를 만나면
+					v[nr][nc] = true;
+					map[nr][nc] = 0;
+					--cheeseCnt;
+				}
+			}
 
+		}
+
+	}
+	
+	static int stoi(String str) {
+		return Integer.parseInt(str);
+	}
 }
