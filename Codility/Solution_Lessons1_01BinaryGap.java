0d98d9ab5c4f5ef2c966c9aab40ab7bcaa8dbf0d
jhk828 <j828hh@gmail.com>
2021-06-26 23:09:29 +0900

    210626 알고리즘

---
+++ Codility/Solution_Lessons1_01BinaryGap.java
@@ -0,0 +1,28 @@
+// 210626
+
+class Solution_Lessons1_01BinaryGap {
+	public int solution(int N) {
+        // 이진수로 변환
+        String binary = Integer.toBinaryString(N);
+
+        int max = 0;
+        for(int i=0; i<binary.length(); i++) {
+            int cur = stoi(binary.substring(i, i+1));
+            if(cur==0) continue;
+            else {
+                for(int j=i+1; j<binary.length(); j++) {
+                    int next = stoi(binary.substring(j, j+1));
+                    if(next==1) {
+                        max = Math.max(max, j-i-1);
+                        break;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    static int stoi(String str) {
+        return Integer.parseInt(str);
+    }
+}
