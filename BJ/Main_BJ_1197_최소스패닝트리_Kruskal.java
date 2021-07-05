a062322449b3b0bd48885404d929340164c7e894
jhk828 <j828hh@gmail.com>
2021-07-05 11:57:03 +0900

    210705 알고리즘

--- BJ/Main_BJ_1197_최소스패닝트리_Kruskal.java
+++ BJ/Main_BJ_1197_최소스패닝트리_Kruskal.java
@@ -1,85 +1,84 @@
 import java.io.*;
 import java.util.*;
-// 210628
+// 210705
 
 public class Main_BJ_1197_최소스패닝트리_Kruskal {
-    // 간선 중심 - 크루스칼 알고리즘 -> 가중치 낮은 순서대로 V-1개의 간선을 택한다.
-    static int V, E;
+    static int V, E; // 정점, 간선 수
     static int[] parents;
     static PriorityQueue<Edge> pq;
-
-    static class Edge implements Comparable<Edge>{
+    static class Edge implements Comparable<Edge> {
         int from, to, w;
-
         public Edge(int from, int to, int w) {
             this.from = from;
             this.to = to;
             this.w = w;
         }
-
         @Override
         public int compareTo(Edge o) {
             return Integer.compare(this.w, o.w);
         }
     }
     public static void main(String[] args) throws Exception {
-        //System.setIn(new FileInputStream("src/res/input.txt"));
+        System.setIn(new FileInputStream("src/res/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         V = stoi(st.nextToken());
         E = stoi(st.nextToken());
-        pq = new PriorityQueue<Edge>();
         parents = new int[V+1];
         for(int i=1; i<=V; i++) {
             parents[i] = i;
         }
+        pq = new PriorityQueue<Edge>();
 
+        // 간선 정보 입력
         for(int i=0; i<E; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int from = stoi(st.nextToken());
             int to = stoi(st.nextToken());
-            int weight = stoi(st.nextToken());
-            pq.offer(new Edge(from, to, weight));
+            int w = stoi(st.nextToken());
+            pq.offer(new Edge(from, to, w));
         }
 
-        int ans = kruskal();
-        System.out.println(ans);
+        int total = kruskal();
+        System.out.println(total);
+
         br.close();
     }
 
     static int kruskal() {
-        // 사이클이 발생하지 않는 V-1개의 간선 구하기
-        int totalWeight = 0;
+        int total = 0;
         int cnt = 0;
-        while (true) {
-            if(cnt==V-1) break;
-            //if(pq.isEmpty()) break;
+
+        // 사이클이 발생하지 않는 정점-1개의 간선 택
+        while(!pq.isEmpty()){
             Edge cur = pq.poll();
-            if(union(cur.from, cur.to)) { // 합집합이 가능-> 사이클x
-                totalWeight += cur.w;
+            if(find(cur.from) != find(cur.to)) {
+                union(cur.from, cur.to);
+                total += cur.w;
                 ++cnt;
             }
+            if(cnt == V-1) break;
         }
-        return totalWeight;
+
+        return total;
     }
 
-    // 1. 부모 루트 찾기
-    static int find(int a) {
-        if(parents[a]==a) return a;
-        return parents[a] = find(parents[a]);
+    static int find(int x){
+        if(parents[x]==x) return x;
+        return parents[x] = find(parents[x]);
     }
 
-    // 2. 합집합 연산이 불가하면 사이클 발생
-    static boolean union(int a, int b) {
-        int aRoot = find(a);
-        int bRoot = find(b);
-        if(aRoot==bRoot) return false;
-        parents[aRoot] = bRoot;
-        return true;
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x!=y) {
+            if(x < y) parents[y] = x;
+            else parents[x] = y;
+        }
     }
 
     static int stoi(String str) {
         return Integer.parseInt(str);
     }
 }
-
