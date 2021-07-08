5d3fc4479531ff25f419ffcefcfd273fdd8bf452
jhk828 <j828hh@gmail.com>
2021-07-08 18:08:10 +0900

    210708 알고리즘

--- BJ/Main_BJ_1303_전쟁전투.java
+++ BJ/Main_BJ_1303_전쟁전투.java
@@ -1,69 +1,75 @@
 import java.io.*;
 import java.util.*;
-// 210529
-public class Main_BJ_1303_전쟁전투 {
-    static char[][] map;
-    static int N, M; // 가로, 세로
-    static int white, blue;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, -1, 0, 1};
-    static boolean[][] v;
-    static Queue<Pos> q;
+// 210708
 
+public class Main_BJ_1303_전쟁전투 {
+    static int N, M, totalW, totalB;
+    static char[][] map;
+    static boolean[][] v;
+    static int[] dr = new int[] {-1, 1, 0, 0};
+    static int[] dc = new int[] {0, 0, -1, 1};
     static class Pos {
         int r, c;
-        public Pos(int r, int c) {
+        public Pos(int r, int c){
             this.r = r;
             this.c = c;
         }
     }
-    public static void main(String[] args) throws Exception {
-        //System.setIn(new FileInputStream("src/res/input_BJ_1303_전쟁전투.txt"));
+    public static void main(String[] args) throws Exception{
+        //System.setIn(new FileInputStream("src/res/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = stoi(st.nextToken()); // 가로
-        M = stoi(st.nextToken()); // 세로
-        map = new char[M][N];
-        white = 0; blue = 0;
-        v = new boolean[M][N];
-        q = new ArrayDeque<>();
+        M = stoi(st.nextToken()); // 가로
+        N = stoi(st.nextToken()); // 세로
+        map = new char[N][M];
+        v = new boolean[N][M];
 
-        for(int i=0; i<M; i++) {
+        for(int i=0; i<N; i++) {
             map[i] = br.readLine().toCharArray();
         }
 
-        for(int i=0; i<M; i++) {
-            for(int j=0; j<N; j++) {
+        totalW = 0;
+        totalB = 0;
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
                 if(!v[i][j]) {
-                    q.offer(new Pos(i, j));
-                    v[i][j] = true;
-                    bfs(map[i][j]);
+                    if (map[i][j]=='W') {
+                        totalW += bfs(i, j);
+                    } else if(map[i][j]=='B') {
+                        totalB += bfs(i, j);
+                    }
                 }
             }
         }
 
-        System.out.println(white + " " + blue);
+        System.out.println(totalW + " " + totalB);
         br.close();
     }
 
-    static void bfs(char color) {
-        int res = 1;
-        while(!q.isEmpty()){
+    static int bfs(int r, int c) {
+        int total = 0, cnt = 1;
+        char color = map[r][c];
+        Queue<Pos> q = new ArrayDeque<Pos>();
+        // 시작점
+        v[r][c] = true;
+        q.offer(new Pos(r, c));
+
+        while(!q.isEmpty()) {
             Pos cur = q.poll();
             for(int d=0; d<4; d++) {
                 int nr = cur.r + dr[d];
                 int nc = cur.c + dc[d];
-                if(nr>=M || nr<0 || nc>=N || nc<0 || map[nr][nc]!=color || v[nr][nc]) continue;
-                q.offer(new Pos(nr, nc));
+                if(nr<0 || nr>=N || nc<0 || nc>=M || v[nr][nc] || map[nr][nc]!=color) continue;
+                ++cnt;
                 v[nr][nc] = true;
-                ++res;
+                q.offer(new Pos(nr, nc));
             }
         }
-        if(color=='W') white += Math.pow(res, 2);
-        else blue += Math.pow(res, 2);
+        total += Math.pow(cnt, 2);
+        return total;
     }
 
-    static int stoi(String str) {
+    static int stoi(String str){
         return Integer.parseInt(str);
     }
 }
