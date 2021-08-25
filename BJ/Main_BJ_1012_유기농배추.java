82f27f92502e5d2b44273c74dc281d896956edc4
jhk828 <j828hh@gmail.com>
2021-08-25 23:48:10 +0900

    210825 알고리즘

--- BJ/Main_BJ_1012_유기농배추.java
+++ BJ/Main_BJ_1012_유기농배추.java
@@ -1,107 +1,85 @@
 import java.io.*;
 import java.util.*;
-
-// 210212
+// 210825
 
 public class Main_BJ_1012_유기농배추 {
+    static int M, N, K; // 가로, 세로
+    static int[][] map;
+    static boolean[][] v;
+    static int[] dr = new int[]{-1, 1, 0, 0};
+    static int[] dc = new int[]{0, 0, -1, 1};
 
-	static int M, N, K;
-	static int[][] map;
-	static boolean[][] visited;
-	static int cnt;
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, -1, 0, 1};
-	static Queue<Integer[]> q;
-	
-	static void printMap() {
-		for(int r=0; r<M; r++) {
-			for(int c=0; c<N; c++) {
-				System.out.print(map[r][c] + " ");
-			}
-			System.out.println();
-		}
-	}
-	
-	static void dfs(int r, int c) {
-		visited[r][c] = true;
-		
-		for(int d=0; d<4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-			
-			if (nr>=0 && nr<M && nc>=0 && nc<N 
-					&& map[nr][nc]==1 && !visited[nr][nc]) {
-				visited[nr][nc] = true;
-				dfs(nr, nc);
-			}
-				
-		}
-	}
-	
-	static void bfs(int r, int c) {
-		q.offer(new Integer[] {r, c});
-		visited[r][c] = true;
-		
-		while (!q.isEmpty()) {
-			Integer[] v = q.poll();
-			
-			for(int d=0; d<4; d++) {
-				int nr = v[0] + dr[d];
-				int nc = v[1] + dc[d];
-				
-				if (nr>=0 && nr<M && nc>=0 && nc<N 
-						&& map[nr][nc]==1 && !visited[nr][nc]) {
-					visited[nr][nc] = true;
-					q.offer(new Integer[] {nr, nc});
-				}
-			}
-		}
-		
-	}
-	
-	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/input_BJ_1012_유기농배추.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc=1; tc<=T; tc++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			M = Integer.parseInt(st.nextToken()); // 가로 길이
-			N = Integer.parseInt(st.nextToken()); // 세로 길이
-			K = Integer.parseInt(st.nextToken()); // 배추 위치 개수
-			
-			map = new int[M][N];
-			visited = new boolean[M][N];
-			cnt = 0;
-			q = new ArrayDeque<Integer[]>();
-			
-			for(int i=0; i<K; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				int r = Integer.parseInt(st.nextToken()); 
-				int c = Integer.parseInt(st.nextToken()); 
-				map[r][c] = 1;
-			}
-			
-			for(int r=0; r<M; r++) {
-				for(int c=0; c<N; c++) {
-					if (map[r][c]==0) {
-						visited[r][c] = true;
-					} else {
-						if (!visited[r][c]) {
-							++cnt;
-							visited[r][c] = true;
-							// dfs(r, c);
-							bfs(r, c);
-						}
-					}
-				}
-			}
+    static class Pos {
+        int r, c;
+        public Pos(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
 
-			System.out.println(cnt);
-		} // for
-		
-		br.close();
-	} // main
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("src/res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = stoi(br.readLine());
+        for(int tc=1; tc<=T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            M = stoi(st.nextToken()); // 가로
+            N = stoi(st.nextToken()); // 세로
+            K = stoi(st.nextToken());
+            int min = 0;
+
+            map = new int[N][M];
+            v = new boolean[N][M];
+
+            for(int i=0; i<K; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int c = stoi(st.nextToken()); // x
+                int r = stoi(st.nextToken()); // y
+                map[r][c] = 1;
+            }
+
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+                    if(map[i][j]==0) {
+                        v[i][j] = true;
+                    } else if(map[i][j]==1 && !v[i][j]) {
+                        ++min;
+                        v[i][j] = true;
+                        bfs(i, j);
+                    }
+                }
+            }
+            System.out.println(min);
+        }
+    }
+
+
+    static void bfs(int r, int c) {
+        Queue<Pos> q = new ArrayDeque<Pos>();
+        // 시작점
+        q.offer(new Pos(r, c));
+        v[r][c] = true;
+
+        while (!q.isEmpty()) {
+            Pos cur = q.poll();
+            for(int d=0; d<4; d++) {
+                int nr = cur.r + dr[d];
+                int nc = cur.c + dc[d];
+                if(!valid(nr, nc) || v[nr][nc] || map[nr][nc]!=1) continue;
+                // 방문
+                v[nr][nc] = true;
+                q.offer(new Pos(nr, nc));
+            }
+        }
+    }
+
+    static boolean valid(int nr, int nc) {
+        if(nr<0 || nr>=N || nc<0 || nc>=M) return false;
+        else return true;
+    }
+
+    static int stoi(String str) {
+        return Integer.parseInt(str);
+    }
 }
