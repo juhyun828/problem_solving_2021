c3526b6655d0da6ef1d58ea74aeb912abc1b08a1
jhk828 <j828hh@gmail.com>
2021-09-01 21:23:40 +0900

    210901 알고리즘

--- Programmers/Solution_LV2_후보키.java
+++ Programmers/Solution_LV2_후보키.java
@@ -1,85 +1,83 @@
 import java.util.*;
-// 210717 
+// 210901
 
 class Solution_LV2_후보키 {
-    static ArrayList<String> candidates; // 순열 결과,후보키리스트
-    static String[][] relation;
-    public int solution(String[][] inputRelation) {
-        int answer = 0;
+    static ArrayList<String > combList, candidates;
+    static int N;
+    static boolean[] v;
 
-        relation = inputRelation;
+    public int solution(String[][] relation) {
+        int answer = 0;
+        N = relation[0].length; //4 0~3 
+
+        combList = new ArrayList<String>();
         candidates = new ArrayList<String>();
+        v = new boolean[N];
+
+        combination(0,  ""); // 0, 1, 2, 01, 012, ,,,
         
-        permutation(0, "", relation[0].length);
+        // Collections.sort(combList, new Comparator<String>(){
+        //     @Override
+        //     public int compare(String o1, String o2) {
+        //         return Integer.compare(o1.length(), o2.length());
+        //     }
+        // });
         
-        Collections.sort(candidates, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2){
-                return Integer.compare(o1.length(), o2.length());
-            }
-        });
-        
-        List<String> list = new ArrayList<String>();
-        for(String c: candidates) {
-            list.add(c);
-        } 
-        
-        for(String c: list) {
-            if(candidates.contains(c)) {
-                candidates = checkMinimality(c);
+        for(String str: combList) {
+            if(checkUnig(str, relation) && checkMin(str)) {
+                candidates.add(str);
             }
         }
-        
+
         return candidates.size();
     }
-    
-    static void permutation(int L, String res, int N) {
+
+    static void combination(int L, String str) {
         if(L==N) {
-            if(res.isEmpty()) return;       
-            if(checkUniqueness(res)) {
-                // 유일성을 만족
-                candidates.add(res);
-            }
+            if(str.length()>0)
+                combList.add(str);
             return;
         }
-        
-        permutation(L+1, res+L, N);
-        permutation(L+1, res, N);
-    }
-    
-    static boolean checkUniqueness(String res) {
-        List<String> list = new ArrayList<String>();
-        int[] cols = new int[res.length()];
-        int idx=0;
-        for(String s: res.split("")) {
-            cols[idx++] = Integer.parseInt(s);
-        }
 
-        for(String[] r: relation) {
-            String str = "";
-            for(int c: cols) {
-                str += r[c];
+        combination(L+1, str);
+        combination(L+1, str + L);
+    }
+
+    // 최소성 검사
+    static boolean checkMin(String str) {
+        for(String key: candidates) { //0 01
+            if(key.length() < str.length()) {
+                // 한 글자씩 따져야 한다. key의 모든 요소가 str에 포함되어 있을 때
+                boolean flag = false;
+                for(String s: key.split("")){
+                    if(!str.contains(s)) {
+                        flag = true;
+                        break;
+                    } 
+                }
+                if(!flag) return false;
             }
-            if(list.contains(str)) return false;
-            else list.add(str);
         }
-        
         return true;
     }
-    
-    static ArrayList<String> checkMinimality(String key) {
-        // candidates복사
-        ArrayList<String> list = new ArrayList<String>();
-        for(String c: candidates) {
-            list.add(c);
+
+    // 유일성 검사
+    static boolean checkUnig(String str, String[][] relation) { //  013
+        String[] arr =  str.split(""); // 013
+        Set<String> set = new HashSet<String>();
+        for(String[] r: relation) {
+            String val = "";
+            for(int i=0; i< arr.length; i++ ) {
+                val += r[stoi(arr[i])];
+            }
+
+            if(val.length()>0 && set.contains(val)) return false;
+            else if(val.length()>0 && !set.contains(val)) set.add(val);
         }
-        
-        // candidates 배열에서 key를 포함하는 문자열을 제거한다.
-        for(String c: candidates) {
-            if(c.equals(key)) continue;
-            if(c.contains(key)) list.remove(c);
-        }
-        
-        return list;
+        return true;
+    }
+
+    static int stoi(String str) {
+        return Integer.parseInt(str);
     }
 }
---
+++ Programmers/Solution_LV2_후보키_fail.java
@@ -0,0 +1,85 @@
+import java.util.*;
+// 210717 
+
+class Solution_LV2_후보키_fail {
+    static ArrayList<String> candidates; // 순열 결과,후보키리스트
+    static String[][] relation;
+    public int solution(String[][] inputRelation) {
+        int answer = 0;
+
+        relation = inputRelation;
+        candidates = new ArrayList<String>();
+        
+        permutation(0, "", relation[0].length);
+        
+        Collections.sort(candidates, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2){
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+        
+        List<String> list = new ArrayList<String>();
+        for(String c: candidates) {
+            list.add(c);
+        } 
+        
+        for(String c: list) {
+            if(candidates.contains(c)) {
+                candidates = checkMinimality(c);
+            }
+        }
+        
+        return candidates.size();
+    }
+    
+    static void permutation(int L, String res, int N) {
+        if(L==N) {
+            if(res.isEmpty()) return;       
+            if(checkUniqueness(res)) {
+                // 유일성을 만족
+                candidates.add(res);
+            }
+            return;
+        }
+        
+        permutation(L+1, res+L, N);
+        permutation(L+1, res, N);
+    }
+    
+    static boolean checkUniqueness(String res) {
+        List<String> list = new ArrayList<String>();
+        int[] cols = new int[res.length()];
+        int idx=0;
+        for(String s: res.split("")) {
+            cols[idx++] = Integer.parseInt(s);
+        }
+
+        for(String[] r: relation) {
+            String str = "";
+            for(int c: cols) {
+                str += r[c];
+            }
+            if(list.contains(str)) return false;
+            else list.add(str);
+        }
+        
+        return true;
+    }
+    
+    static ArrayList<String> checkMinimality(String key) {
+        // candidates복사
+        ArrayList<String> list = new ArrayList<String>();
+        for(String c: candidates) {
+            list.add(c);
+        }
+        
+        // candidates 배열에서 key를 포함하는 문자열을 제거한다.
+        for(String c: candidates) {
+            if(c.equals(key)) continue;
+            if(c.contains(key)) list.remove(c);
+        }
+        
+        return list;
+    }
+}
